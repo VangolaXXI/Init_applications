e70907a09408826a0d5fbe11978d5c5d6c133479
VangolaXXI <samidin_1989@mail.ru>
2024-02-16 11:08:25 +0600

    Настройка стилей

--- app/src/main/java/com/example/init/TwoActivity.java
+++ app/src/main/java/com/example/init/TwoActivity.java
@@ -1,5 +1,9 @@
 package com.example.init;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TwoActivity extends AppCompatActivity {
@@ -10,6 +14,15 @@ public class TwoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
 
+        Button loginButton = findViewById(R.id.loginButton1);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Обработчик нажатия на кнопку
+                Intent intent = new Intent(TwoActivity.this, SmartIQ.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
