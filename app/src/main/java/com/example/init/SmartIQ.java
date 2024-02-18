ab197f0a89887209955bcc9dd0a85572acab4e81
VangolaXXI <samidin_1989@mail.ru>
2024-02-18 12:00:08 +0600

    Добавления логики в главное меню

--- app/src/main/java/com/example/init/SmartIQ.java
+++ app/src/main/java/com/example/init/SmartIQ.java
@@ -2,7 +2,10 @@ package com.example.init;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SmartIQ extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class SmartIQ extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smart_iq);
+
+        Button buttonMain = findViewById(R.id.bt_main);
+        buttonMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Создать Intent для открытия другой активити
+                Intent intent = new Intent(SmartIQ.this, persanal_card.class);
+                // Запустить активити
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
